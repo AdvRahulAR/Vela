1c8af36a60e2a5312c8d44e528b40d1817c9b933
AdvRahulAR <advrahular@gmail.com>
2026-09-23 16:27:38 +0530

    release: bump version to 0.2.1

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -22,7 +22,7 @@ export const DEFAULT_TOKEN_TYPES: EntityType[] = [
   'CUSTOM',
 ];
 
-export const PACKAGE_VERSION = '0.2.0';
+export const PACKAGE_VERSION = '0.2.1';
 
 export const DIGITS = '0123456789';
 export const UPPER_ALNUM = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
